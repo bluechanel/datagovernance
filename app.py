d09e22396d4b912b4f1dd53361ce6266050fa551
Wiley <1321969685@qq.com>
2021-09-01 15:22:12 +0800

    update directory structure

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import datetime
+import logging
+import os
 
 from flask import Flask, render_template, jsonify, request, send_from_directory
 import pandas as pd
@@ -13,7 +15,7 @@ def hello_world():
 
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_from_directory('static/data', filename)
+    return send_from_directory('data', filename)
 
 
 def allowed_file(file_name: str):
@@ -38,8 +40,8 @@ def clean(path: str) -> list[str]:
     n = data.isnull().any(axis=1)
     m = n[n is True].index
     result2_data = data[data.index.isin(m)]
-    result1_data.to_excel(f'static/data/{result1_name}')
-    result2_data.to_excel(f'static/data/{result2_name}')
+    result1_data.to_excel(f'data/{result1_name}')
+    result2_data.to_excel(f'data/{result2_name}')
     return [result1_name, result2_name]
 
 
@@ -60,8 +62,8 @@ def finance(path1: str, path2: str) -> list[str]:
     result2_data = pd.merge(data2, data1, how='outer', on="卡号", indicator=True).query('_merge == "left_only"').drop(
         columns=['_merge'])
 
-    result1_data.to_excel(f'static/data/{result1_name}')
-    result2_data.to_excel(f'static/data/{result2_name}')
+    result1_data.to_excel(f'data/{result1_name}')
+    result2_data.to_excel(f'data/{result2_name}')
     return [result1_name, result2_name]
 
 
@@ -82,21 +84,23 @@ def ledger(path1: str, path2: str) -> list[str]:
     # 差集
     result2_data = pd.merge(data2, data1, how='outer', on="IP", indicator=True).query('_merge == "left_only"').drop(
         columns=['_merge'])
-    result1_data.to_excel(f'static/data/{result1_name}')
-    result2_data.to_excel(f'static/data/{result2_name}')
+    result1_data.to_excel(f'data/{result1_name}')
+    result2_data.to_excel(f'data/{result2_name}')
     return [result1_name, result2_name]
 
 
 @app.post('/data_clean')
 def clean_route():
-    s = request.get_json()
-    result_name = clean(s["path"])
+    r = request.get_json()
+    logging.info(str(r))
+    result_name = clean(r["path"])
     return jsonify({"filename": f"{result_name}"})
 
 
 @app.post('/finance')
 def finance_route():
     r = request.get_json()
+    logging.info(str(r))
     result_name = finance(r["path1"], r["path2"])
     return jsonify({"filename": result_name})
 
@@ -104,9 +108,18 @@ def finance_route():
 @app.post('/ledger')
 def ledger_route():
     r = request.get_json()
+    logging.info(str(r))
     result_name = ledger(r["path1"], r["path2"])
     return jsonify({"filename": result_name})
 
 
+def mkdir(dirs: str):
+    if not os.path.exists(dirs):
+        os.makedirs(dirs)
+
+
 if __name__ == '__main__':
+    logging.warning("请不要在使用该工具时关闭当前命令行窗口！")
+    logging.warning("使用浏览器访问http://127.0.0.1:5000")
+    # mkdir('static/data/')
     app.run()
