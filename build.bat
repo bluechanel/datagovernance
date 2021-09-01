ce5945932a449bdcf8b6549cac362e2ef4a45bdd
Wiley <1321969685@qq.com>
2021-09-01 14:44:04 +0800

    clean

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 title=building
 @echo off
-pyinstaller -F --noupx -i static/img/data.ico .\app.py
+pyinstaller --clean -y -F --noupx -i static/img/data.ico .\app.py --add-data "static;static" --add-data "templates;templates
 pause
 exit
