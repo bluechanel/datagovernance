e62bf6ba17129ff6be16cde5a5f68899c01127a5
Wiley <1321969685@qq.com>
2021-09-13 15:00:06 +0800

    新增内容

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ import webbrowser
 app = Flask(__name__)
 
 
+def judge(col1, col2):
+    return "一致" if col1 == col2 else "不一致"
+
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
@@ -23,28 +27,35 @@ def allowed_file(file_name: str):
     return '.' in file_name and file_name.rsplit('.', 1)[1] in ['xlsx', 'xls']
 
 
-def clean(path: str) -> list[str]:
+def clean(path: str, dup_col: list[str], na_col: list[str]) -> list[str]:
     """
     数据清洗函数
+    :param na_col:
+    :param dup_col:
     :param path:
     :return:
     """
     result1_name = f"重复_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
     result2_name = f"空字段_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    result3_name = f"清洗完成_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
     data = pd.read_excel(path)
-    # 重复  根据id去重
-    data_duplicates = data.drop_duplicates(subset=["卡号"], keep=False)
+    # 重复  根据卡号去重
+    data_duplicates = data.drop_duplicates(subset=dup_col, keep=False)
     result1_data = pd.merge(data, data_duplicates, how='outer', on="卡号", indicator=True).query(
         '_merge == "left_only"').drop(
         columns=['_merge'])
     # 有空值  所有字段空值判断
-    n = data.isnull().any(axis=1)
+    tmp_data = data.loc[:, na_col]
+    n = tmp_data.isnull().any(axis=1)
     # == 是因为这里是numpy
     m = n[n == True].index
     result2_data = data[data.index.isin(m)]
+    result3_data = data.drop_duplicates(subset=dup_col, keep='first')
+    result3_data = result3_data.dropna(subset=na_col)
     result1_data.to_excel(f'data/{result1_name}')
     result2_data.to_excel(f'data/{result2_name}')
-    return [result1_name, result2_name]
+    result3_data.to_excel(f'data/{result3_name}')
+    return [result1_name, result2_name, result3_name]
 
 
 def finance(path1: str, path2: str) -> list[str]:
@@ -54,8 +65,9 @@ def finance(path1: str, path2: str) -> list[str]:
     :param path2:bill
     :return:
     """
-    result1_name = f"信息账单交集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
-    result2_name = f"账单信息差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    result1_name = f"账单中不在系统的卡_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    result2_name = f"卡号一致明细表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    result3_name = f"系统中不在账单卡_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
     data1 = pd.read_excel(path1)
     data2 = pd.read_excel(path2)
     # 交集
@@ -63,38 +75,68 @@ def finance(path1: str, path2: str) -> list[str]:
     # 差集
     result2_data = pd.merge(data2, data1, how='outer', on="卡号", indicator=True).query('_merge == "left_only"').drop(
         columns=['_merge'])
-
+    result3_data = pd.merge(data1, data2, how='outer', on="卡号", indicator=True).query('_merge == "left_only"').drop(
+        columns=['_merge'])
     result1_data.to_excel(f'data/{result1_name}')
     result2_data.to_excel(f'data/{result2_name}')
-    return [result1_name, result2_name]
+    result3_data.to_excel(f'data/{result3_name}')
+    return [result1_name, result2_name, result3_name]
 
 
-def ledger(path1: str, path2: str) -> list[str]:
+def ledger(path1: str, path2: str, col: str) -> list[str]:
     """
     信息台账表合并函数
+    :param col:
     :param path1: info
     :param path2: ledger
     :return:
     """
-    result1_name = f"信息台账差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
-    result2_name = f"台账信息差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    if col == "IP":
+        result1_name = f"信息台账差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+        result2_name = f"台账信息差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+        result3_name = f"台账信息交集拼接_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    else:
+        result1_name = f"表1表2差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+        result2_name = f"表2表1差集表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+        result3_name = f"表1表2交集拼接_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
     data1 = pd.read_excel(path1)
     data2 = pd.read_excel(path2)
     # 交集
-    result1_data = pd.merge(data1, data2, how='outer', on="IP", indicator=True).query('_merge == "left_only"').drop(
+    result1_data = pd.merge(data1, data2, how='outer', on=col, indicator=True).query('_merge == "left_only"').drop(
         columns=['_merge'])
     # 差集
-    result2_data = pd.merge(data2, data1, how='outer', on="IP", indicator=True).query('_merge == "left_only"').drop(
+    result2_data = pd.merge(data2, data1, how='outer', on=col, indicator=True).query('_merge == "left_only"').drop(
         columns=['_merge'])
+    result3_data = pd.merge(data1, data2, how='inner', on=col)
     result1_data.to_excel(f'data/{result1_name}')
     result2_data.to_excel(f'data/{result2_name}')
+    result3_data.to_excel(f'data/{result3_name}')
+    return [result1_name, result2_name, result3_name]
+
+
+def diff(path1: str, path2: str) -> list[str]:
+    result1_name = f"费用一致表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    result2_name = f"费用不一致表_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.xlsx"
+    data1 = pd.read_excel(path1)
+    data2 = pd.read_excel(path2)
+    merge_data = pd.merge(data1, data2, how='outer', on="卡号")
+    print(merge_data)
+    merge_data["说明"] = merge_data.apply(lambda x: judge(x.费用_x, x.费用_y), axis=1)
+    print(merge_data)
+    result1_data = merge_data[merge_data['说明'] == '一致']
+    result2_data = merge_data[merge_data['说明'] == '不一致']
+    result1_data.to_excel(f'data/{result1_name}')
+    result2_data.to_excel(f'data/{result2_name}')
+
     return [result1_name, result2_name]
 
 
 @app.post('/data_clean')
 def clean_route():
     r = request.get_json()
-    result_name = clean(r["path"])
+    dup_col = r["dup_col"].split(",")
+    na_col = r["na_col"].split(",")
+    result_name = clean(r["path"], dup_col, na_col)
     return jsonify({"filename": result_name})
 
 
@@ -108,7 +150,21 @@ def finance_route():
 @app.post('/ledger')
 def ledger_route():
     r = request.get_json()
-    result_name = ledger(r["path1"], r["path2"])
+    result_name = ledger(r["path1"], r["path2"], "IP")
+    return jsonify({"filename": result_name})
+
+
+@app.post('/merge')
+def merge_route():
+    r = request.get_json()
+    result_name = ledger(r["path1"], r["path2"], r["col"])
+    return jsonify({"filename": result_name})
+
+
+@app.post('/diff')
+def diff_route():
+    r = request.get_json()
+    result_name = diff(r["path1"], r["path2"])
     return jsonify({"filename": result_name})
 
 
